bf86fe485a559e31e067311609dfbdf439821922
Yorke <zc_and_zc@aliyun.com>
2014-11-30 13:02:18 +0800

    init

---
+++ HealthTipSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthTipSwift
+//
+//  Created by Yorke on 14/11/30.
+//  Copyright (c) 2014年 Yorke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
